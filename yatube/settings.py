6c35a995ba8a6586510fd28c4ce09f162d826c4d
Vitaly Sofronyuk <68146917+Gilions@users.noreply.github.com>
2020-09-23 20:48:19 +0300

    Update settings.py

--- yatube/settings.py
+++ yatube/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = [
